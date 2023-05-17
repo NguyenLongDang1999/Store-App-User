ed1b08f3adcf8a01506c5a4e3d9cb6a1a4fd5f9d
nguyenlongdang0412 <130326222+nguyenlongdang0412@users.noreply.github.com>
2023-05-17 09:13:19 +0700

    Remove Package

--- plugins/vue3-carousel.ts
+++
@@ -1,9 +0,0 @@
-import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
-
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.vueApp.component('Slide', Slide)
-    nuxtApp.vueApp.component('Carousel', Carousel)
-    nuxtApp.vueApp.component('Navigation', Navigation)
-    nuxtApp.vueApp.component('Pagination', Pagination)
-})
